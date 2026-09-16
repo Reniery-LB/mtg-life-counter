0414b564d3cd5abc8c6b3ba700783c24230856e1
Reniery-lb <relu_23@alu.uabcs.mx>
2026-09-15 20:24:19 -0700

    feat: Magic The Gathering

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import { useState } from 'react'
+import '../App.css';
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
+  const [player1, setPlayer1] = useState(20);
+  const [player2, setPlayer2] = useState(20);
+
+  const decreasePlayer1 = () => {
+    if (player1 > 0)
+      setPlayer1(player1 - 1);
+  }
+
+  const decreasePlayer2 = () => {
+    if (player2 > 0)
+      setPlayer2(player2 - 1);
+  }
+
+  const reset = () => {
+    const confirmar = window.confirm("¿Estas seguro de reiniciar los puntos?");
+    if (confirmar) {
+      setPlayer1(20);
+      setPlayer2(20);
+    }
+  }
 
   return (
-    <div>
-      <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter - 1)}>-</button>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter(0)}>Reset</button>
+    <div className="app">
+      <h1 className="title">Magic The Gathering</h1>
+      <br />
+      <h1>Player 1: {player1}</h1>
+      <button className="btn" onClick={decreasePlayer1}>-</button>
+      <button className="btn" onClick={() => setPlayer1(player1 + 1)}>+</button>
+
+      <h1>Player 2: {player2}</h1>
+      <button className="btn" onClick={decreasePlayer2}>-</button>
+      <button className="btn" onClick={() => setPlayer2(player2 + 1)}>+</button>
+
+      <br />
+      <button className="reset" onClick={reset}>Reiniciar</button>
+
     </div>
   )
 }
